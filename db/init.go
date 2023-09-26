b72364b36f78f1ef6297886db2944bf8868fa156
XiaoFan <1066580119@qq.com>
2023-09-26 21:52:25 +0800

    update:改用sqlite

--- db/init.go
+++ db/init.go
@@ -1,30 +1,33 @@
 package db
 
 import (
-	"Axiss_server/config"
-	"fmt"
-	"gorm.io/driver/mysql"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var GlobalDb *gorm.DB
 
 func init() {
-	globalConfig := config.GetGlobalConfig()
+	//globalConfig := config.GetGlobalConfig()
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/Axiss?charset=utf8mb4&parseTime=True&loc=Local",
-		globalConfig.Mysql.User,
-		globalConfig.Mysql.Password,
-		globalConfig.Mysql.Addr)
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s)/Axiss?charset=utf8mb4&parseTime=True&loc=Local",
+	//	globalConfig.Mysql.User,
+	//	globalConfig.Mysql.Password,
+	//	globalConfig.Mysql.Addr)
 
 	//dsn := "myuser:123123123@tcp(110.42.182.92:3307)/Axiss?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("axiss.db"), &gorm.Config{})
 	if err != nil {
+		//log.Errorln("无法连接到数据库",dsn)
+		log.Errorln("无法连接到数据库:", "sqlite")
 		panic("无法连接到数据库")
 	}
 	err = db.AutoMigrate(&Feed{}, &BenchMark{})
 	if err != nil {
+		log.Errorln("数据库迁移失败:", "sqlite")
 		panic("数据库迁移失败")
 	}
 	GlobalDb = db
--- go.mod
+++ go.mod
@@ -13,7 +13,8 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.5.1
-	gorm.io/gorm v1.25.1
+	gorm.io/driver/sqlite v1.5.3
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55
 )
 
 require (
@@ -49,6 +50,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
